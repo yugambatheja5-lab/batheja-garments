769601cf41b5043ce72c6b9482b152558a549093
Yugam Batheja <yugambatheja5@gmail.com>
2026-07-21 18:23:20 +0530

    Use Render port

--- backend/server.js
+++ backend/server.js
@@ -717,8 +717,10 @@ app.get('/', (req, res) => {
   res.send("Backend running with DB 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
 
